1ebad842fcd22ed29173575a88851832937229bc
Enrico Erler <enrico.erler@xebia.com>
2025-02-17 12:10:16 +0100

    Implement RestAPI and CLI as workspace packages
    Using the [tool.uv.workspace] will separate the package logic from its use again.

---
+++ packages/app/src/app/__init__.py
@@ -0,0 +1,9 @@
+import uvicorn
+import time
+
+def main() -> None:
+    print("Setting up uvicorn server...")
+    time.sleep(5)
+    uvicorn.run("app.app:app", reload=True)
+    
+    
---
+++ packages/cli/src/cli/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Run (uv run) animal-cli to use the cli!")
