2cd1eef8df4ab82c5605358d559c22026308ab3e
tintinrevient <zhaobenben007@googlemail.com>
2025-12-12 18:10:26 +0100

    Update generator answer 2 and 3

--- notebooks/generators_iterators/answers/ex-gen-build3.py
+++ notebooks/generators_iterators/answers/ex-gen-build3.py
@@ -3,3 +3,8 @@ def fibonacci_numbers(nums):
     for _ in range(nums):
         x, y = y, x+y
         yield x
+
+fibonacci_gen = fibonacci_numbers(10)
+
+for fibonacci_number in fibonacci_gen:
+    print(fibonacci_number)
