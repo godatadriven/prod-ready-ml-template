5158b84c2125f0b7f8451a32ad77b7fa493fa5b0
tintinrevient <zhaobenben007@googlemail.com>
2025-12-12 18:13:44 +0100

    Update generator answer 2

--- notebooks/generators_iterators/answers/ex-gen-use2.py
+++ notebooks/generators_iterators/answers/ex-gen-use2.py
@@ -2,4 +2,4 @@ def get_alphabet_letter(alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
     for ind, letter in enumerate(alphabet):
         yield (letter, ind+1)
 
-[x.lower() if x in 'AEIOU' else x for x in get_alphabet_letter()]
+[x for x in get_alphabet_letter() if x[0] in 'AEIOU']
